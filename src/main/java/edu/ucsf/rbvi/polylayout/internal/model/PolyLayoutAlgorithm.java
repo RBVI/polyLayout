1c76752dc86fbb44b0f80cd12665e95625a0a6b6
Hanan <Hanan@10.131.63.155>
2017-06-15 15:48:06 -0700

    Here is my experimental PolyLayoutAlgorithm that should currently work for n = 2 (n number of categories)

---
+++ src/main/java/edu/ucsf/rbvi/polylayout/internal/model/PolyLayoutAlgorithm.java
@@ -0,0 +1,40 @@
+package edu.ucsf.rbvi.polylayout.internal.model;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.cytoscape.model.CyNode;
+import org.cytoscape.service.util.CyServiceRegistrar;
+import org.cytoscape.view.model.CyNetworkView;
+import org.cytoscape.view.model.View;
+import org.cytoscape.view.presentation.property.BasicVisualLexicon;
+
+public class PolyLayoutAlgorithm {	
+	public static void doLayout(Map<Object, Double> sizeMap, 
+			Map<Object, List<View<CyNode>>> nodeMap, 
+			CyServiceRegistrar reg, CyNetworkView networkView, final int spacing) {
+		if(sizeMap == null || nodeMap == null || networkView == null)
+			return;
+		else {
+			if(sizeMap.size() != 2)
+				return;
+			else {
+				Collection<View<CyNode>> collecOfNodes = networkView.getNodeViews();
+				double x = 0;
+				for(Object categoryKey : nodeMap.keySet()) {
+					List<View<CyNode>> nodesInCategory = nodeMap.get(categoryKey);
+					double y = 0;
+					double yOffSet = 0;
+					for(View<CyNode> nodeView : nodesInCategory) {
+						y += yOffSet;
+						nodeView.setVisualProperty(BasicVisualLexicon.NODE_X_LOCATION, x);
+						nodeView.setVisualProperty(BasicVisualLexicon.NODE_Y_LOCATION, y + nodeView.getVisualProperty(BasicVisualLexicon.NODE_SIZE));
+						yOffSet = spacing + nodeView.getVisualProperty(BasicVisualLexicon.NODE_SIZE);
+					}
+					x += 200;
+				}
+			}
+		}
+	}
+}
